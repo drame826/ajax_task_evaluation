e2bf354dd7f7d41b2b1ff104022f38e335823209
drame826 <dianguineds04@gmail.com>
2023-04-02 15:09:09 +0000

    first

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,38 +1,18 @@
 class FavoritesController < ApplicationController
   before_action :set_blog
 
-  #def create
-   # current_user.favorites.create(blog_id: params[:blog_id])
-  #end
-
   def create
-    @favorite = @blog.favorites.build(comment_params)
-    respond_to do |format|
-      if @favorite.save
-        flash.now[:notice] = 'コメントが投稿されました'
-        format.js { render :index }
-      else
-        flash.now[:notice] = 'コメントの投稿に失敗しました'
-        format.js { render :index }
-      end
-    end
+    current_user.favorites.create(blog_id: params[:blog_id])
   end
 
- # def destroy
-  #  current_user.favorites.find_by(id: params[:id]).destroy
-  #end
-
   def destroy
-    @favorite = favorite.find(params[:id])
-      @favorite.destroy!
-      respond_to do |format|
-        flash.now[:notice] = 'コメントが削除されました'
-        format.js { render :index }
-      end
+    current_user.favorites.find_by(id: params[:id]).destroy
   end
 
   private
   def set_blog
     @blog = Blog.find(params[:blog_id])
   end
+
+
 end
